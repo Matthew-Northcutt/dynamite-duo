6d510c2e38af74e2458588f82f3798d62bbe3257
Matthew Northcutt <matthew.northcutt0813@gmail.com>
2024-08-01 09:51:41 -0500

    Added HTML rendering module

--- scripts/movie-converter.js
+++ scripts/movie-converter.js
@@ -9,7 +9,8 @@ export const generateMoviesHTML = () => {
         //For each movie, we generate an HTML structure using template literals:
 
 /*We create an <article> element with the class "movie" to represent each movie card.
-Inside the <article>, we have an <img> element that displays the movie poster image. The src attribute is set to the poster property of the movie object, and the alt attribute is set to the movie title followed by "poster" for accessibility.
+Inside the <article>, we have an <img> element that displays the movie poster image. 
+The src attribute is set to the poster property of the movie object, and the alt attribute is set to the movie title followed by "poster" for accessibility.
 We also have a <div> element with the class "movie__details" to hold the movie title and description.
 The movie title is wrapped in an <h2> element with the class "movie__title".
 The movie description is wrapped in a <p> element with the class "movie__description".*/
@@ -32,3 +33,13 @@ movieHTML += `
  module.exports = {
     generateMoviesHTML
  }
+/*we created a JavaScript module that converts the movie data into HTML markup. 
+By importing the movie data from the movies-data.js file,
+we can access the movies array and iterate over each movie object.
+Inside the generateMoviesHTML function, we use a for...of loop to go through each movie object and 
+generate the corresponding HTML structure using template literals.
+We dynamically insert the movie poster, title, and description into the appropriate elements.
+The generated HTML markup for each movie is concatenated into the movieHTML string, 
+which is then returned by the function. This allows us to obtain the complete HTML markup for all the movies.
+By exporting the generateMoviesHTML function, we make it available for use in other modules, 
+such as the main module where we will render the movie cards to the DOM.*/
